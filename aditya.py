04e5e93b2ddaa920e9a32077fd8d898d4835aab3
aditya <jadona815@gmail.com>
2025-08-19 16:05:59 +0530

    Add files via upload

---
+++ aditya.py
@@ -0,0 +1,18 @@
+# task-1
+a=int(input("enter a first no:"))
+b=int(input("enter a second no:"))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("addition is:",addition)
+print("subtraction is:",subtraction)
+print("multiplication is:",multiplication)
+print("division is:",division)
+
+#task-2
+first_name=(input("enter your first name:"))
+last_name=(input("enter your last name:"))
+full_name=first_name+" "+last_name
+print(f"\nhello,{full_name}!welcome to the python program")
+
